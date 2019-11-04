4a299ba2cb31a4713dbe48edf7fbd9be4f72910b
Nahid Kalam <nkalam12@outlook.com>
2019-11-04 23:07:41 +0000

    Added javascript (click handler, conditional)

--- js/script.js
+++ js/script.js
@@ -4,8 +4,26 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+$("#shoot").click(function(){
+    let player = $("#input").val();
+    let compute = Math.random(0, 2);
+    if (compute === 0){
+        let computer = "rock";
+    } else if (compute === 1) {
+        let computer = "paper";
+    }
 
 
+
+});
+
+$
 
 // DOCUMENT READY FUNCTION BELOW
 
+
+
+
+
+
+//python3 -m http.server 3000
